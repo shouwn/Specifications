ad778d4a5323d97f69f678c4fea6954669d67ef6
shouwn <sjonghyon@gmail.com>
2018-11-06 19:13:56 +0900

    성공

---
+++ sample/src/main/generated/com/shouwn/specifications/sample/specifications/SampleModelSpecifications.java
@@ -0,0 +1,9 @@
+package com.shouwn.specifications.sample.specifications;
+
+import java.lang.String;
+import lombok.Data;
+
+@Data
+public class SampleModelSpecifications {
+  private String test;
+}
--- sample/src/main/java/com/shouwn/specifications/sample/Sample.java
+++ sample/src/main/java/com/shouwn/specifications/sample/Sample.java
@@ -1,8 +1,10 @@
 package com.shouwn.specifications.sample;
 
+import com.shouwn.specifications.sample.specifications.SampleModelSpecifications;
+
 public class Sample {
 
     public static void main(String[] args){
-
+        new SampleModelSpecifications();
     }
 }
